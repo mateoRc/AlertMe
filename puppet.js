8f9954899feafcd4a5fec3b180a595d893017c8f
mateo <mmateo19992@gmail.com>
2020-06-13 21:55:04 +0200

    ho

--- puppet.js
+++ puppet.js
@@ -1,14 +1,8 @@
 const PATH_TO_CHROME = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe';
 const URL = 'http://localhost:3333/';
 
-const puppeteer = require('puppeteer');
-
-async function triggerEmail() {
-    console.log('sending email...');
-}
-
-(async () => {
-
+let res = (async () => {
+    const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({
         executablePath: PATH_TO_CHROME,
         devtools: true,
@@ -25,23 +19,40 @@ async function triggerEmail() {
     page.once('domcontentloaded', () => console.info('✅ DOM is ready'));
 
     try {
-        await page.evaluate(() => {
+        let res1 = await page.evaluate(() => {
             const listElementObserver = new MutationObserver((mutation) => {
 
                 mutation.forEach((mutation) => {
                     if (mutation.type === 'childList') {
-                        console.log('Alert, new element inside forEach'); 
-                        triggerEmail(); 
+                        console.log('new Element!'); //  prints in devtools!
+                        return 1; 
                     } 
                 });
 
             });
+
+            // choose element to evaluate
             const msgArea = document.getElementById('message-area');
             listElementObserver.observe(msgArea, {attributes: true, childList: true, subtree: true})
         });
+
+        // test
+        console.log('res1' + res1);
+        return res1;
     } catch (e) {
         console.log(e);
     }
-    
-    
 })();
+
+/* TODO
+console.log(res1)
+(async (res) => {
+    if (res = 1) {
+        console.log('new element!');
+    }
+})();
+
+function triggerEmail() {
+    console.log('it works! (te)');
+}
+*/
