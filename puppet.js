b6692ca3623bac74d4600ee3141bb57fbd26ced1
mateo <mmateo19992@gmail.com>
2020-06-14 21:37:34 +0200

    asd

--- puppet.js
+++ puppet.js
@@ -1,8 +1,9 @@
 const PATH_TO_CHROME = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe';
 const URL = 'http://localhost:3333/';
+const puppeteer = require('puppeteer');
+var nodemailer = require('nodemailer');
 
-let res = (async () => {
-    const puppeteer = require('puppeteer');
+(async () => {
     const browser = await puppeteer.launch({
         executablePath: PATH_TO_CHROME,
         devtools: true,
@@ -19,23 +20,53 @@ let res = (async () => {
     page.once('domcontentloaded', () => console.info('✅ DOM is ready'));
 
     try {
-        let res1 = await page.evaluate(() => {
+        await page.exposeFunction('triggerEmail', triggerEmail);
+
+        await page.evaluate(() => {
             const listElementObserver = new MutationObserver((mutation) => {
 
                 mutation.forEach((mutation) => {
                     if (mutation.type === 'childList') {
-                        console.log('new <li> Element!'); // prints in devtools! 
-                    } 
+                        console.log('new Message!'); // prints in devtools!
+                        triggerEmail();
+                    }
                 });
-
             });
-
             // choose element to evaluate
             const msgArea = document.getElementById('message-area');
-            listElementObserver.observe(msgArea, {attributes: true, childList: true, subtree: true})
+            listElementObserver.observe(msgArea, {
+                attributes: true, 
+                childList: true, 
+                subtree: true
+            });
         });
-
     } catch (e) {
         console.log(e);
     }
 })();
+
+function triggerEmail() {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: '',
+          pass: ''
+        }
+    });
+    
+    var mailOptions = {
+    from: 'sender@gmail.com',
+    to: 'mateo19992@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+    });
+}
+
